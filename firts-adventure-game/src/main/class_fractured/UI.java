dd936d9047df8552126e20ad11bbf2f7a13c215d
kaloati92 <kaloati92@gmail.com>
2024-04-16 16:14:24 +0200

    Multiple classes 1 11:00

--- firts-adventure-game/src/main/class_fractured/UI.java
+++ firts-adventure-game/src/main/class_fractured/UI.java
@@ -14,8 +14,18 @@ public class UI {
     private JFrame window;
     private JPanel titleNamePanel;
     private JPanel startButtonPanel;
+    private JPanel mainTextPanel;
+    private JPanel choiceButtonPanel;
+    private JPanel playerPanel;
     private JLabel titleNameLabel;
+    private JLabel hpLabel;
+    private JLabel hpNumberLabel;
     private JButton startButton;
+    private JButton choice1;
+    private JButton choice2;
+    private JButton choice3;
+    private JButton choice4;
+    private JTextArea mainTextArea;
     private Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
     private Font normalFont = new Font("Times New Roman", Font.PLAIN, 26);
 
@@ -48,6 +58,76 @@ public class UI {
 
         window.add(titleNamePanel);
         window.add(startButtonPanel);
+
+        //Game Screen
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.black);
+        window.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Placeholder for mainTextArea");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextArea.setWrapStyleWord(true);
+        mainTextArea.setEditable(false);
+        mainTextPanel.add(mainTextArea);
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
+        choiceButtonPanel.setBackground(Color.black);
+        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        window.add(choiceButtonPanel);
+
+        choice1 = new JButton("choice1");
+        choice1.setBackground(Color.black);
+        choice1.setForeground(Color.white);
+        choice1.setFont(normalFont);
+        choice1.setFocusPainted(false);
+//        choice1.setActionCommand("c1");
+        choiceButtonPanel.add(choice1);
+
+        choice2 = new JButton("choice2");
+        choice2.setBackground(Color.black);
+        choice2.setForeground(Color.white);
+        choice2.setFont(normalFont);
+        choice2.setFocusPainted(false);
+//        choice2.setActionCommand("c2");
+        choiceButtonPanel.add(choice2);
+
+        choice3 = new JButton("choice3");
+        choice3.setBackground(Color.black);
+        choice3.setForeground(Color.white);
+        choice3.setFont(normalFont);
+        choice3.setFocusPainted(false);
+//        choice3.setActionCommand("c3");
+        choiceButtonPanel.add(choice3);
+
+        choice4 = new JButton("choice4");
+        choice4.setBackground(Color.black);
+        choice4.setForeground(Color.white);
+        choice4.setFont(normalFont);
+        choice4.setFocusPainted(false);
+//        choice4.setActionCommand("c4");
+        choiceButtonPanel.add(choice4);
+
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100, 15, 600, 50);
+        playerPanel.setBackground(Color.black);
+        playerPanel.setLayout(new GridLayout(1, 4));
+        window.add(playerPanel);
+
+        hpLabel = new JLabel("HP:");
+        hpLabel.setFont(normalFont);
+        hpLabel.setForeground(Color.white);
+        playerPanel.add(hpLabel);
+
+        hpNumberLabel = new JLabel();
+        hpNumberLabel.setFont(normalFont);
+        hpNumberLabel.setForeground(Color.white);
+        playerPanel.add(hpNumberLabel);
 
         window.setVisible(true);
     }
